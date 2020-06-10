cb2b792d0f42f36efbf792e8ca4f1b19716f9535
Elena <eispravnikova@gpm-rtv.ru>
2020-06-10 22:58:12 +0300

    завкршает дз1

--- js/main.js
+++ js/main.js
@@ -1,32 +1,70 @@
 'use strict';
-var objMap = {
-  author: {
-    avatar:["img/avatars/user01.png", "img/avatars/user02.png","img/avatars/user03.png","img/avatars/user04.png","img/avatars/user05.png", "img/avatars/user06.png", "img/avatars/user07.png", "img/avatars/user08.png"]
-  },
-  offer: {
-    title: ["заголовок #1", "заголовок #2", "заголовок #3","заголовок #4", "заголовок #5","заголовок #6","заголовок #7", "заголовок #8"],
-    address:["100, 350",  "200, 350",  "300, 350",  "400, 350",  "500, 350", "600, 350", "500, 250", "500, 450"],
-    price: [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000],
-    type: ["palace", "bungalo", "bungalo", "flat", "bungalo", "house", "bungalo", "house"],
-    rooms: [1, 2, 3, 4, 5, 6, 7, 8],
-    guests: [1, 2, 3, 4, 5, 6, 7, 8],
-    checkin: ["12:00", "13:00", "12:00", "13:00","14:00", "13:00","12:00", "14:00"],
-    checkout: ["12:00", "13:00", "14:00", "13:00","12:00", "14:00","12:00", "13:00"],
-    features: ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"],
-    description: ["описание #1", "описание #2", "описание #3", "описание #4", "описание #5", "описание #6", "описание #7", "описание #8"],
-    photos: ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg", "http://o0.github.io/assets/images/tokyo/hotel4.jpg", "http://o0.github.io/assets/images/tokyo/hotel5.jpg", "http://o0.github.io/assets/images/tokyo/hotel6.jpg", "http://o0.github.io/assets/images/tokyo/hotel7.jpg","http://o0.github.io/assets/images/tokyo/hotel8.jpg"]
-  },
-  location: {
-    x: [200, 300, 100, 500, 110, 250, 590, 380],
-    y: [130, 200, 300, 350, 400, 450, 500, 550]
-  }
-};
+var TITLE = ['заголовок #1', 'заголовок #2', 'заголовок #3', 'заголовок #4'];
+var TYPE = ['palace', 'flat', 'bungalo', 'house'];
+var TIMES = ['12:00', '13:00', '14:00'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var NUMBER_OF_PINS = 8;
 
-var pools = document.querySelectorAll('.map__pins');
-var template = document.querySelector('#pin').content.querySelector('.map__pin');
+var map = document.querySelectorAll('.map__pins');
+var pin = document.querySelector('#pin').content.querySelector('.map__pin');
 
-for (var i = 0; i < 8; i++) {
-  var element = template.cloneNode(true);  
-  element.setAttribute('style','left:'+objMap.location.x[i]+'px;'+'top:'+ objMap.location.y[i] +'px');
-  pools[0].appendChild(element);
+//Функция гененрации случайного числа от и до
+var makeRandomNumber = function(min, max) {
+  var i = Math.floor(Math.random() * (max-min)) + min;
+  return i
 }
+
+//Функция гененрации случайного элемента в масситве
+var makeRandomElement = function(array) {
+  var i = Math.floor(Math.random() * array.length)
+  var element = array[i]
+  return element
+}
+
+//Функция для создания массива
+var createArray = function() {
+  var location = {
+    x: makeRandomNumber(0, map[0].offsetWidth - 50),
+    y: makeRandomNumber(130, 630)
+  };
+  var newArray = {
+    author: {
+      avatar: 'img/avatars/user0' + makeRandomNumber(1,NUMBER_OF_PINS) + '.png'
+    },
+    offer: {
+      title: makeRandomElement(TITLE),
+      address: location.x + ',' + location.y,
+      price: makeRandomNumber(1000, 50000),
+      type: makeRandomElement(TYPE),
+      rooms: makeRandomNumber(1, 3),
+      guest: makeRandomNumber(0, 3),
+      checkin: makeRandomElement(TIMES),
+      checkout: makeRandomElement(TIMES),
+      features: makeRandomElement(FEATURES),
+      description: makeRandomElement(TITLE),
+      photos: makeRandomElement(PHOTOS)
+    },
+    location: location
+  };
+  return newArray;
+}
+
+//Функция для генерации меток на карте
+var createMapPins = function() {  
+  for (var i = 0; i < NUMBER_OF_PINS; i++) {
+    var obj = createArray()
+    var mapElement = pin.cloneNode(true);
+    //модификация атрибутов
+    mapElement.style.left = obj.location.x + 'px';
+    mapElement.style.top = obj.location.y + 'px';
+    mapElement.querySelector('img').src = obj.author.avatar;
+    mapElement.querySelector('img').alt = obj.offer.title;
+    //добавление метки в div
+    map[0].appendChild(mapElement);
+    console.log(obj);
+  }
+}
+
+//Заполняем блок элементами
+createMapPins();
