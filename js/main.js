cc56811b042a41a03aba33b8dfea2be4d3577d54
Elena <eispravnikova@gpm-rtv.ru>
2020-06-27 21:46:01 +0300

    coordinate

--- js/main.js
+++ js/main.js
@@ -97,6 +97,7 @@ var blockMap = document.querySelector('.map');
 var blockAdForm = document.querySelector('.ad-form ');
 var elementsFieldset = document.querySelectorAll('fieldset');
 var elementsSelect = document.querySelectorAll('select');
+var elementsAddress = document.querySelector('#address');
 
 //функция удаления и добавлениия атрибута disabled
 var disabledForm = function (x) {
@@ -118,18 +119,44 @@ var disabledForm = function (x) {
     return false
   }
 }
-//добавляем атрибуты disabled
-disabledForm('add');
 
-buttonPinMain.addEventListener('mousedown', function () {
-  if (blockMap.classList.contains('map--faded') && blockAdForm.classList.contains('ad-form--disabled')) {
-    blockMap.classList.remove('map--faded');
-    blockAdForm.classList.remove('ad-form--disabled');
-    //удаляематрибут атрибуты disabled
-    disabledForm('remove');
-    //// Заполняем блок map элементами
-    createMapPins(NUMBER_OF_PINS);
+//функция активации карты
+var activePage = function() {
+  blockMap.classList.remove('map--faded');
+  blockAdForm.classList.remove('ad-form--disabled');
+  //удаляем атрибуты disabled
+  disabledForm('remove');
+  //Заполняем блок map элементами
+  createMapPins(NUMBER_OF_PINS);
+}
+
+//функция определения координат метки
+var addСoordinates = function(map) {
+  //высота псевдоэлемента after
+  var pathMap = 16;
+  if (map == 'center') {
+    elementsAddress.value = Math.floor((buttonPinMain.offsetLeft + buttonPinMain.offsetWidth/2)) + ', ' + Math.floor((buttonPinMain.offsetTop + buttonPinMain.offsetHeight/2));
   } else {
-    return false
+    elementsAddress.value = Math.floor((buttonPinMain.offsetLeft + buttonPinMain.offsetWidth/2)) + ', ' +  Math.floor((buttonPinMain.offsetTop + buttonPinMain.offsetHeight + pathMap));
+  }
+}
+
+//добавляем атрибуты disabled при загрузке страницы
+disabledForm('add');
+addСoordinates('center');
+
+//При клике активизируется карта
+buttonPinMain.addEventListener('mousedown', function (evt) {
+  if (blockMap.classList.contains('map--faded') && blockAdForm.classList.contains('ad-form--disabled') && (evt.buttons == 1)) {
+    activePage();
+    addСoordinates();
+  }
+});
+
+//При нажатии "enter" активизируется карта
+buttonPinMain.addEventListener('keydown', function (evt) {
+  if (blockMap.classList.contains('map--faded') && blockAdForm.classList.contains('ad-form--disabled') && (evt.key === 'Enter')) {
+    activePage();
+    addСoordinates();
   }
 });
