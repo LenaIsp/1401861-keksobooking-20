4081579317778ecbdc2c79371b015b20c59b555b
Elena <eispravnikova@gpm-rtv.ru>
2020-07-13 20:59:49 +0300

    new module

---
+++ js/data.js
@@ -0,0 +1,53 @@
+'use strict';
+(function () {
+  var TITLE = ['Заголовок #1', 'Заголовок #2', 'Заголовок #3', 'Заголовок #4'];
+  var DESCRIPTION = ['Описание #1', 'Описание #2', 'Описание #3', 'Описание #4'];
+  var TYPE = ['palace', 'flat', 'bungalo', 'house'];
+  var TIMES = ['12:00', '13:00', '14:00'];
+  var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+  var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+  var NUMBER_OF_PINS = 8;
+
+  // Функция гененрации случайного числа от и до
+  var makeRandomNumber = function (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
+
+  // Функция гененрации случайного элемента в масситве
+  var makeRandomElement = function (array) {
+    var i = Math.floor(Math.random() * array.length);
+    return array[i];
+  };
+
+  //доступ к переменным и функции из любого файла
+  window.data = {
+    NUMBER_OF_PINS: NUMBER_OF_PINS,
+    // Функция для создания массива
+    createArray: function () {
+      var location = {
+        x: makeRandomNumber(0, document.querySelector('.map__pins').offsetWidth - 50),
+        y: makeRandomNumber(130, 630)
+      };
+      var check = makeRandomElement(TIMES);
+      return {
+        author: {
+          avatar: 'img/avatars/user0' + makeRandomNumber(1, NUMBER_OF_PINS) + '.png'
+        },
+        photos: PHOTOS,
+        offer: {
+          title: makeRandomElement(TITLE),
+          address: location.x + ', ' + location.y,
+          price: makeRandomNumber(1000, 5000),
+          type: makeRandomElement(TYPE),
+          rooms: makeRandomNumber(1, 3),
+          guests: makeRandomNumber(0, 3),
+          checkin: check,
+          checkout: check,
+          features: makeRandomElement(FEATURES),
+          description: makeRandomElement(DESCRIPTION),
+        },
+        location: location
+      };
+    }
+  };
+})();
---
+++ js/pin.js
@@ -0,0 +1,51 @@
+'use strict';
+(function () {
+  // элементы 
+  var map = document.querySelectorAll('.map__pins');
+  var pin = document.querySelector('#pin').content.querySelector('.map__pin');
+  var card = document.querySelector('.map__filters-container');
+  var cardInfo = document.querySelector('#card').content.querySelector('.map__card');
+  var popupPhoto = document.querySelector('#card').content.querySelector('.popup__photos > img');
+  // Функция для генерации меток на карте
+  var createMapPins = function (number) {
+    for (var i = 0; i < number; i++) {
+      var obj = window.data.createArray();
+      var mapElement = pin.cloneNode(true);
+      var cardElement = cardInfo.cloneNode(true);
+
+      // модификация атрибутов в шаблоне "pin"
+      mapElement.style.left = obj.location.x + 'px';
+      mapElement.style.top = obj.location.y + 'px';
+      mapElement.querySelector('img').src = obj.author.avatar;
+      mapElement.querySelector('img').alt = obj.offer.title;
+      mapElement.style.display = 'none';
+
+      // модификация атрибутов в шаблоне "card"
+      cardElement.querySelector('.popup__title').innerHTML = obj.offer.title;
+      cardElement.querySelector('.popup__text--address').innerHTML = obj.offer.address;
+      cardElement.querySelector('.popup__text--price').innerHTML = obj.offer.price + '₽/ночь';
+      cardElement.querySelector('.popup__text--capacity').innerHTML = obj.offer.rooms + ' комнаты для ' + obj.offer.guests + ' гостей';
+      cardElement.querySelector('.popup__text--time').innerHTML = 'Заезд после ' + obj.offer.checkin + ', выезд до ' + obj.offer.checkout;
+      cardElement.querySelector('.popup__description').innerHTML = obj.offer.description;
+      cardElement.querySelector('.popup__avatar').src = obj.author.avatar;
+      cardElement.style.display = 'none';
+      // находим фото
+      var blocks = cardElement.querySelectorAll('.popup__photos > img');
+      // удаляем ненужное фото
+      cardElement.querySelector('.popup__photos').removeChild(blocks[0]);
+      // заполняем фотографиями из массива
+      for (var j = 0; j < obj.photos.length; j++) {
+        var cardPhoto = popupPhoto.cloneNode(true);
+        cardPhoto.src = obj.photos[j];
+        cardElement.querySelector('.popup__photos').appendChild(cardPhoto);
+      }
+      // добавление метки в блок "map__pins"
+      map[0].appendChild(mapElement);
+
+      // создание 8 карточек перед блоком "map__filters-container"
+      card.before(cardElement);
+    }
+  };
+  // создаем элементы при загрузке страницы с атрибутом disabled
+  createMapPins(window.data.NUMBER_OF_PINS);
+})();
