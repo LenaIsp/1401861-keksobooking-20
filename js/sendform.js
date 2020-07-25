eb0d29593a2ea453fe45cc0fb00a566d46122887
Elena <eispravnikova@gpm-rtv.ru>
2020-07-26 00:03:22 +0300

    неактивные поля формы

--- js/sendform.js
+++ js/sendform.js
@@ -21,6 +21,7 @@
     var cloneSuccess = success.cloneNode(true);
     var pinThumbnails = document.querySelectorAll('.map__pin');
     var cardThumbnails = document.querySelectorAll('.map__card');
+    window.map.disabledForm('add');
     document.body.appendChild(cloneSuccess);
 
     cloneSuccess.addEventListener('click', function () {
@@ -46,16 +47,20 @@
     var errorButton = cloneError.querySelector('.error__button');
     cloneError.querySelector('.error__message').innerText = message;
     document.body.appendChild(cloneError);
+    window.map.disabledForm('add');
     errorButton.addEventListener('click', function () {
       cloneError.remove();
+      window.map.disabledForm('remove');
     });
     cloneError.addEventListener('click', function () {
       cloneError.remove();
+      window.map.disabledForm('remove');
     });
     var onSuccessEscPress = function (evt) {
       if (evt.key === 'Escape') {
         evt.preventDefault();
         cloneError.remove();
+        window.map.disabledForm('remove');
         document.removeEventListener('keydown', onSuccessEscPress);
       }
     };
